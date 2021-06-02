9db6763564f2acc91050a211e07174ec1abf0d13
javascript-lang-ctrl <80439575+javascript-lang-ctrl@users.noreply.github.com>
2021-06-02 13:47:20 -0500

    Add files via upload

---
+++ hoi.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $characterName = "John";
+    $favoriteWord = "hoi";
+    $characterAge = "90";
+    echo("There Was a Person named $characterName.");
+    echo("His Favorite Word Was $favoriteWord.");
+    echo("He was $characterAge Years old.");
+    ?>
+</body>
+</html>
